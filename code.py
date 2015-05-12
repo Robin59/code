d6a05663e75e71e7c3ff6b696cf3821afd06738d
robin59 <robingiraudon@hotmail.fr>
2015-05-12 06:44:50 +0200

    Add the vigenere's method

--- code.py
+++ code.py
@@ -2,7 +2,7 @@
 
 
 
-
+#Cesar's method
 def cesarCode(string,key):
     res = ''
     for char in string :
@@ -15,11 +15,46 @@ def cesarDecode(string,key):
     	res+=chr(ord(char)-key)
     return res
 
+#Vigenere's method
+def vigCode(string,key) :
+    """
+    Code a message by using vigenere's method, the key must be an iterable
+    """
+    res=''
+    length=len(key)
+    i=0
+    for char in string :
+        res+=chr(ord(char)+key[i%length])
+        i+=1
+    return res
+
+def vigDecode(string,key) :
+    """
+    Decode a message by using vigenere's method, the key must be an iterable
+    """
+    res=''
+    length=len(key)
+    i=0
+    for char in string :
+        res+=chr(ord(char)-key[i%length])
+        i+=1
+    return res
+
+
 #test
+
+print 'cesar code with value=a1234 and key=1'
 codeValue = cesarCode('a1234',1)
 baseValue = cesarDecode(codeValue,1)
-print 'cesar code with value=a1234 and key=1'
 print 'code :'
 print codeValue
 print 'decode :'
 print baseValue
+print 'Vigenere code with value=a1234 and key=[1,2]'
+codeValue = vigCode('a1234',[1,2])
+baseValue = vigDecode(codeValue,[1,2])
+print 'code :'
+print codeValue
+print 'decode :'
+print baseValue
+
