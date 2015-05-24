9b09a4e264dc7f60d7970fc9b84ae945c58978c0
robin59 <robingiraudon@hotmail.fr>
2015-05-25 00:30:16 +0200

    Add tests for Vigenere and Enigma Cyphers

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ def createRandomString() :
 Create a random string
     """
     string=''
+    for i in range(randint(0,300)) :
+        string+=(chr(randint(0,255)))
     return string
 
 class TestCesar(unittest.TestCase):
@@ -16,11 +18,39 @@ class TestCesar(unittest.TestCase):
         self.assertEquals(cesarDecode('b2345',1), 'a1234');
 
     def testWithRandomValue(self):
-        string='rttsqzeq'
+        string=createRandomString()
         key=randint(0,255)
         code=cesarCode(string,key)
         self.assertEquals(cesarDecode(code,key),string)
 
+class TestVigenere(unittest.TestCase):
+    
+    def testBase(self):
+        self.assertEquals(vigCode('a1234',[1,2]), 'b3355');
+        self.assertEquals(vigDecode('b3355',[1,2]), 'a1234');
+
+    def testWithRandomValue(self):
+        string=createRandomString()
+        key= tools.vigKey(randint(1,10))
+        code=vigCode(string,key)
+        self.assertEquals(vigDecode(code,key),string)
+
+
+class TestEnigma(unittest.TestCase):
+    
+    def testBase(self):
+        string='abc'
+        key=[[],[]]
+        for x in range(0,256):
+            key[0].append((x+2)%256)
+            key[1].append((x+4)%256)
+        self.assertEquals(enigmaCode(string,key),'gik')
+
+    def testWithRandomValue(self):
+        string=createRandomString()
+        key= tools.enigmaKey(randint(0,5))
+        code=enigmaCode(string,key)
+        self.assertEquals(enigmaDecode(code,key),string)
 
 
 
